34b290ba1f1adf298e42ec68374faf99c0819029
luizguilh3rme <luizguilhermelg107@gmail.com>
2024-08-29 05:37:47 -0300

    att

--- Potencia_Tech_Ifood-DIO/src/JSON/json.js
+++ Potencia_Tech_Ifood-DIO/src/JSON/json.js
@@ -7,7 +7,31 @@ generateInvoice(name,products, productsValue,age)
 function generateInvoice(name, products, productsValue, age){
     console.log(`O comprador é ${name}`)
     console.log(`A idade é ${age}`)
-    console.log(`---------------------`)
     console.log(`O produto é ${products[0]}`)
     console.log(`O valor é ${productsValue[0]}`)
+    console.log(`---------------------`)
+}
+
+let invoice ={
+    name:"felipe",
+    age: 28,
+    products: {
+        0: ["mouse 2xmw", 29.34],
+        1: ["mouse teclado", 245.34],
+        2: ["monitor", 346.87]
+    },
+    taxes: "56.76"
+}
+
+generateInvoice2(invoice)
+
+function generateInvoice2(invoice){
+    console.log(`O comprador é ${invoice.name}`)
+    console.log(`A idade é ${invoice.age}`)
+    
+    //for in é o ideal para percorrer objetos json
+    for (let index in invoice.products){
+        let [productsName, productPrice] = invoice.products[index]
+        console.log(`- ${productsName}: R$ ${productPrice}`)
+    }
 }
